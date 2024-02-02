9c3a47a2b3e84cace432f9c995e5aa829577a547
Thiago Veronezi <tveronezi@gmail.com>
2024-02-02 17:16:38 -0500

    save

--- _deploy_nextcloud.sh
+++ _deploy_nextcloud.sh
@@ -8,4 +8,7 @@ echo "###########################################################"
 echo "###########################################################"
 echo ""
 
+# kubectl apply -f ./config/nextcloud_self_signed_cert.yaml
+kubectl apply -f ./config/nextcloud_ingress.yaml
+
 helm upgrade --values ./config/nextcloud_values.yaml --install nextcloud nextcloud/nextcloud
--- start.sh
+++ start.sh
@@ -4,15 +4,11 @@ set -e
 ./_kill_cluster.sh 
 ./_create_cluster.sh 
 ./_setup_ingress.sh 
+
 ./_create_nextcloud_mariadb_secret.sh 
 ./_create_nextcloud_mariadb_volume.sh 
 ./_deploy_nextcloud_mariadb.sh
 
-./_create_nextcloud_volume.sh 
 ./_create_nextcloud_secret.sh 
+./_create_nextcloud_volume.sh 
 ./_deploy_nextcloud.sh
-
-
-
-
-

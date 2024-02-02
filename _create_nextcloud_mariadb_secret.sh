db6874639fc7e93084cfb7623e584fbdc4023b4e
Thiago Veronezi <tveronezi@gmail.com>
2024-02-02 10:02:49 -0500

    blankline

--- _create_nextcloud_mariadb_secret.sh
+++ _create_nextcloud_mariadb_secret.sh
@@ -8,4 +8,3 @@ kubectl get secret "$SECRET_NAME" \
 || kubectl create secret generic "$SECRET_NAME" \
     --from-literal=mariadb-root-user=root \
     --from-literal=mariadb-root-password=$DEV_NEXTCLOUD_MARIADB_ROOT_PASSWORD
-    

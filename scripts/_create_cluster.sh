90da9a9bf738d400ca9ca523e05c22e5c78e7449
Thiago Veronezi <tveronezi@gmail.com>
2024-02-02 22:49:56 -0500

    adjust path

--- scripts/_create_cluster.sh
+++ scripts/_create_cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ./scripts/_env.sh
-kind create cluster --name $CLUSTER_NAME --config ../config/kind_cluster.yaml --wait 1m
+kind create cluster --name $CLUSTER_NAME --config ./config/kind_cluster.yaml --wait 1m

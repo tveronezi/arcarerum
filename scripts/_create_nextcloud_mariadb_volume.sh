cacc7e82b2682bd235c8cd082cc048bfdb3141ab
Thiago Veronezi <tveronezi@gmail.com>
2024-02-02 23:00:03 -0500

    adjust path

--- scripts/_create_nextcloud_mariadb_volume.sh
+++ scripts/_create_nextcloud_mariadb_volume.sh
@@ -2,5 +2,5 @@
 
 source ./scripts/_env.sh
 
-kubectl apply -f ../config/nextcloud_mariadb_pv.yaml
+kubectl apply -f ./config/nextcloud_mariadb_pv.yaml
 kubectl apply -f ./config/nextcloud_mariadb_pvc.yaml
--- scripts/_create_nextcloud_volume.sh
+++ scripts/_create_nextcloud_volume.sh
@@ -2,5 +2,5 @@
 
 source ./scripts/_env.sh
 
-kubectl apply -f ../config/nextcloud_pv.yaml
-kubectl apply -f ../config/nextcloud_pvc.yaml
+kubectl apply -f ./config/nextcloud_pv.yaml
+kubectl apply -f ./config/nextcloud_pvc.yaml
